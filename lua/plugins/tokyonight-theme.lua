395741f63eebe5ad49e89f4897fcacc07511ff85
glennprays <glennsagala@gmail.com>
2024-06-30 14:27:34 +0700

    custom background color

--- lua/plugins/tokyonight-theme.lua
+++ lua/plugins/tokyonight-theme.lua
@@ -2,6 +2,12 @@ return {
   {
     "folke/tokyonight.nvim",
     lazy = true,
-    opts = { style = "night" },
+    opts = {
+      style = "night",
+      on_colors = function(colors)
+        colors.bg = "#0B0C13"
+        colors.bg_sidebar = "#0A0B14"
+      end,
+    },
   },
 }
