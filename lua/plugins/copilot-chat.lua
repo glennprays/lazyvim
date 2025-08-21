2b8cf9cdc2e330b48da7c72caa2c740803c7817b
glennprays <glennsagala@gmail.com>
2025-08-21 15:02:17 +0700

    feat(copilot-chat): add keymap for commit msg on selection
    Added <leader>zs keybinding to generate a commit message for the selected
    code using Copilot Chat. This enhances workflow by allowing quick commit
    message generation directly from visual mode selection.

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -8,10 +8,40 @@ return {
     build = "make tiktoken", -- Only on MacOS or Linux
     opts = {
       -- See Configuration section for options
+
+      separator = "...",
+
+      window = {
+        width = 0.3,
+      },
+      show_folds = false,
+    },
+
+    keys = {
+      { "<leader>zc", ":CopilotChat<CR>", mode = "n", desc = "Chat with Copilot" },
+      { "<leader>zq", ":CopilotChatClose<CR>", mode = "n", desc = "Close Copilot Chat" },
+      { "<leader>zM", ":CopilotChatModels<CR>", mode = "n", desc = "List Copilot Chat Models" },
+      { "<leader>ze", ":CopilotChatExplain<CR>", mode = "v", desc = "Explain Code with Copilot Chat" },
+      { "<leader>zr", ":CopilotChatReview<CR>", mode = "v", desc = "Review Code with Copilot Chat" },
+      { "<leader>zR", ":CopilotChatReset<CR>", mode = "n", desc = "Reset Copilot Chat window" },
+      { "<leader>zf", ":CopilotChatFix<CR>", mode = "v", desc = "Fix Code with Copilot Chat" },
+      { "<leader>zo", ":CopilotChatOptimize<CR>", mode = "v", desc = "Optimize Code with Copilot Chat" },
+      { "<leader>zd", ":CopilotChatDocs<CR>", mode = "v", desc = "Generate Docs with Copilot Chat" },
+      { "<leader>zt", ":CopilotChatTest<CR>", mode = "v", desc = "Generate Tests with Copilot Chat" },
+      { "<leader>zz", ":CopilotChatToggle<CR>", mode = "n", desc = "Toggle Copilot Chat" },
+      { "<leader>zm", ":CopilotChatCommit<CR>", mode = "n", desc = "Generate Commit Message with Copilot Chat" },
+      { "<leader>zS", ":CopilotChatStop<CR>", mode = "n", desc = "Stop Running Copilot Chat" },
+      {
+        "<leader>zs",
+        ":CopilotChatCommit<CR>",
+        mode = "v",
+        desc = "Generate Commit Message for Selection with Copilot Chat",
+      },
     },
     -- See Commands section for default commands if you want to lazy load on them
-    config = function()
-      require("CopilotChat").setup()
+    -- config = function()
+    config = function(_, opts)
+      require("CopilotChat").setup(opts)
     end,
   },
 }
